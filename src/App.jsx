34d4d6e7cd1d1f00ea3ad901491870a2a05bc3b3
Denis <dzianis.pipko@gmail.com>
2025-06-05 16:35:41 +0300

    adding a custom hook to localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import JournalAddButton from './components/JournalAddButton/JournalAddButton'
@@ -6,52 +5,28 @@ import JournalForm from './components/JournalForm/JournalForm'
 import JournalList from './components/JournalList/JournalList'
 import Body from './layouts/Body/Body'
 import LeftPanel from './layouts/LeftPanel/LeftPanel'
+import useLokalStorage from './hooks/use-localstorage.hook'
 
-const INITIAL_DATA = [
-    {
-      id: 1,
-      title: 'title1',
-      date: new Date(),
-      text: 'post1',
-      tag: 'tag',
-    },
-    {
-      id: 2,
-      title: 'title2',
-      date: new Date(),
-      text: 'post2',
-      tag: 'tag',
-    },
-  ]
-
+const mapItems = (items) => {
+  if(!items){
+    return []
+  }
+  return items.map(i => ({
+    ...i,
+    date: new Date(i.date),
+  }))
+}
 function App() {
 
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('data'))
-    if(data){
-      setItems(data.map(item => ({
-        ...item,
-        date: new Date(item.date)
-      })));
-    }
-  }, [])
-
-  useEffect(() => {
-    if(items.length){
-      console.log('Запись');
-      localStorage.setItem('data', JSON.stringify(items))
-    }
-  }, [items])
+  const [items, setItems] = useLokalStorage('data');
 
   const addItem = (item) => {
-    setItems(oldItems => [...oldItems, {
+    setItems([...mapItems(items), {
       title: item.title,
       date: new Date(item.date),
       text: item.text,
       tag: item.tag,
-      id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1
+      id: items.length > 0 ? Math.max(...items.map(i => i.id)) + 1 : 1
     }])
   }
 
@@ -60,7 +35,7 @@ function App() {
       <LeftPanel>
         <Header />
         <JournalAddButton />
-        <JournalList items={items} />
+        <JournalList items={mapItems(items)} />
       </LeftPanel>
       <Body>
         <JournalForm onSubmit={addItem}/>
---
+++ src/hooks/use-localstorage.hook.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+
+const useLokalStorage = (key) => {
+
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        const res = JSON.parse(localStorage.getItem(key))
+        if(res){
+        setData(res);
+        }
+    }, []);
+
+    const saveData = (newData) => {
+        localStorage.setItem(key, JSON.stringify(newData))
+        setData(newData);
+    }
+
+    return [data, saveData]
+}
+
+export default useLokalStorage;
